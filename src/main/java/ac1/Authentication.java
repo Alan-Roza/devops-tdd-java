4058d4f03ff2cc7b2773530fb6788fe7f9572812
Alan Roza <alancruz@outlook.com>
2023-10-01 23:23:56 -0300

    fix

--- src/main/java/ac1/Authentication.java
+++ src/main/java/ac1/Authentication.java
@@ -26,7 +26,7 @@ public class Authentication {
 			return new ValidationResult(false, "Usuário e Senha são obrigatórios");		
 		}
 		
-		if (!user.trim().isEmpty() || !password.trim().isEmpty()) {
+		if (user.trim() != "" || password.trim() != "") {
 			for (AuthUser authUser : getDatabaseUsers()) {
 				if (authUser.getUser().equals(user.trim())) {
 					if (authUser.getFailTries() >= limitTriesToBlockUser) {
