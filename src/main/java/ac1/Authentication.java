d7dcf1114344e8e8bfe31e7c439cc6c7010f7328
Alan Roza <alancruz@outlook.com>
2023-10-01 23:26:35 -0300

    fix

--- src/main/java/ac1/Authentication.java
+++ src/main/java/ac1/Authentication.java
@@ -26,18 +26,22 @@ public class Authentication {
 			return new ValidationResult(false, "Usuário e Senha são obrigatórios");		
 		}
 		
-		if (user.trim() != "" || password.trim() != "") {
+		if (!user.trim().isEmpty() || !password.trim().isEmpty()) {
 			for (AuthUser authUser : getDatabaseUsers()) {
-				if (authUser.getUser().equals(user.trim())) {
-					if (authUser.getFailTries() >= limitTriesToBlockUser) {
-						return new ValidationResult(false, "Credencial bloqueada temporariamente, tente novamente em 3 horas.");
-					}
-					if (authUser.getPassword().equals(password.trim())) {					
-						authUser.setFailTries(0);
-						return new ValidationResult(true, null);
-					}
-					authUser.setFailTries(authUser.getFailTries()+1);	
+				if (!authUser.getUser().equals(user.trim())) {
+					return new ValidationResult(false, "Usuário ou Senha inválidos!");
 				}
+				
+				if (authUser.getFailTries() >= limitTriesToBlockUser) {
+					return new ValidationResult(false, "Credencial bloqueada temporariamente, tente novamente em 3 horas.");
+				}
+				
+				if (authUser.getPassword().equals(password.trim())) {					
+					authUser.setFailTries(0);
+					return new ValidationResult(true, null);
+				}
+				
+				authUser.setFailTries(authUser.getFailTries()+1);	
 			}
 			
 			return new ValidationResult(false, "Usuário ou Senha inválidos!");
